94a8268bcb93f43b298642056e5bac95ca7b4dd5
Philip Levy <philipdlevy@gmail.com>
2022-05-04 16:04:21 -0600

    commit tree 2392217da608347d56fe8bad36ecb38bf108578c

---
+++ src/context/user.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+// create the context
+const UserContext = React.createContext();
+
+
+// create a provider component
+function UserProvider({ children }) {
+  const [user, setUser] = useState(null);
+  return (
+    <UserContext.Provider value={{ user, setUser }}>
+      {children}
+    </UserContext.Provider>
+  );
+}
+
+
+export { UserContext, UserProvider };
